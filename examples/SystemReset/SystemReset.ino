7d95f51c91ffdcbbcf04ef094309d010e77d6c0e
lanselambor <757016515@qq.com>
2018-10-19 12:39:43 +0800

    feat: add SystemReset.ino example

---
+++ examples/SystemReset/SystemReset.ino
@@ -0,0 +1,16 @@
+
+#include <Arduino.h>
+#include <ublox_sara_r4.h>
+
+Ublox_sara_r4 nbIoT = Ublox_sara_r4();
+
+void setup() {
+
+}
+
+void loop() {
+  Log_info("Start");
+  delay(2000);
+
+  nbIoT.SystemReset();
+}
